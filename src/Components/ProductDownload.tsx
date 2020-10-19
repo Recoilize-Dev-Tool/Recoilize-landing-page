01aae88d591dafc8983e5267c0db8819fe3bb478
aaronyang024@gmail.com <aaronyang024@gmail.com>
2020-10-19 15:39:30 -0700

    Updated GettingStarted section to have a slideshow

--- src/Components/ProductDownload.tsx
+++ src/Components/ProductDownload.tsx
@@ -6,7 +6,7 @@ export default function ProductDownload() {
       <div className='inner'>
         <h1>A Chrome extension that helps debug Recoil applications by memorizing the state of components with every render.</h1>
         <div className='buttons'>
-           <a href='' className='button'>Get Started</a>
+           <a href='#getting-started' className='button'>Get Started</a>
            <a href='https://chrome.google.com/webstore/detail/recoilize/jhfmmdhbinleghabnblahfjfalfgidik?hl=en' target='_blank' className='button'>Download</a>
         </div>
         <p>Recoil debugging tool to help developers log state change, travel back to previous 
