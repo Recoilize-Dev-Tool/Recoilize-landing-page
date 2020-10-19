d4c1e750f2a5b54185e2c0bd3e1740761478d9a1
davidemmolino <davidewebdev@gmail.com>
2020-10-19 09:44:05 -0700

    images properly rendering on the DOM

--- src/Components/Team.tsx
+++ src/Components/Team.tsx
@@ -1,78 +1,87 @@
 import React from 'react';
-import saejin from '../assets/saejin.png'
-import davide from '../assets/davide.png'
+import saejin from '../assets/saejin.jpeg';
+import jones from '../assets/jones.png';
+import bren from '../assets/bren.jpeg';
+import sean from '../assets/sean.png';
+import henry from '../assets/henry.jpeg';
+import steven from '../assets/steven.jpeg';
+import spenser from '../assets/spenser.jpeg';
+import seungho from '../assets/seungho.jpeg';
+import justin from '../assets/justin.jpeg';
+import anthony from '../assets/anthony.png';
+import aaron from '../assets/aaron.png';
+import taven from '../assets/taven.png';
+import jesus from '../assets/jesus.png';
+import davide from '../assets/davide.png';
 
 export default function Team() {
     const pictures = [
-        <img src={`${saejin}`} alt="no"/>,
-        <img src={`${davide}`} alt="yes"/>
-        // { 
-        //     src : '../assets/saejin.png',
-        //     alt : 'saejin'
-        // },
-        // {
-        //     src : '../assets/jones.png',
-        //     alt : 'jones'
-        // },
-        // {
-        //     src : '../assets/bren.png',
-        //     alt : 'bren'
-        // },
-        // {
-        //     src : '../assets/sean.png',
-        //     alt : 'sean'
-        // },
-        // {
-        //     src : '../assets/henry.png',
-        //     alt : 'henry'
-        // },
-        // {
-        //     src : '../assets/steven.png',
-        //     alt : 'steven'
-        // },
-        // {
-        //     src : '../assets/spenser.png',
-        //     alt : 'spenser'
-        // },
-        // {
-        //     src : '../assets/seungho.png',
-        //     alt : 'seungho'
-        // },
-        // {
-        //     src : '../assets/justin.png',
-        //     alt : 'justin'
-        // },
-        // {
-        //     src : '../assets/anthony.png',
-        //     alt : 'anthony'
-        // },
-        // {
-        //     src : '../assets/aaron.png',
-        //     alt : 'aaron'
-        // },
-        // {
-        //     src : '../assets/jesus.png',
-        //     alt : 'jesus'
-        // },
-        // {
-        //     src : '../assets/taven.png',
-        //     alt : 'taven'
-        // },
-        // {
-        //     src : '../assets/davide.png',
-        //     alt : 'davide'
-        // }
+        { 
+            src : saejin,
+            alt : 'saejin'
+        },
+        {
+            src : jones,
+            alt : 'jones'
+        },
+        {
+            src : bren,
+            alt : 'bren'
+        },
+        {
+            src : sean,
+            alt : 'sean'
+        },
+        {
+            src : henry,
+            alt : 'henry'
+        },
+        {
+            src : steven,
+            alt : 'steven'
+        },
+        {
+            src : spenser,
+            alt : 'spenser'
+        },
+        {
+            src : seungho,
+            alt : 'seungho'
+        },
+        {
+            src : justin,
+            alt : 'justin'
+        },
+        {
+            src : anthony,
+            alt : 'anthony'
+        },
+        {
+            src : aaron,
+            alt : 'aaron'
+        },
+        {
+            src : jesus,
+            alt : 'jesus'
+        },
+        {
+            src : taven,
+            alt : 'taven'
+        },
+        {
+            src : davide,
+            alt : 'davide'
+        }
     ];
 
-    // pictures.map((pic, i) => {
-    //     console.log(pic.src)
-    //     return <img src={pic.src} alt={pic.alt} key={`${pic.alt}-${i}`} />
-    // })
     return (
         <div>
             <h2 className="section-title-right">Contributors</h2>
             <div id="profile-pictures">
-            {pictures}
+                {pictures.map((pic, i) => {
+                    console.log(pic.src)
+                    return <img src={pic.src} alt={pic.alt} key={`${pic.alt}-${i}`} />
+                })}
             </div>
         </div>
     )
