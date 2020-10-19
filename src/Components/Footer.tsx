cc058c279f71108af426191971ae584fadc7f97c
davidemmolino <davidewebdev@gmail.com>
2020-10-19 12:53:58 -0700

    added links to contributors github and linkedin profiles

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div id="footer">
             Recoilize is an open source project. Licensed under MIT License.
             <br/>
-            <a className="github-button" href="https://github.com/ntkme/github-buttons" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star ntkme/github-buttons on GitHub">Star</a>        
+            <a className="github-button" href="https://github.com/open-source-labs/Recoilize" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star Recoilize/github-buttons on GitHub">Star</a>        
         </div>
     )
 }
--- src/Components/Team.tsx
+++ src/Components/Team.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import saejin from '../assets/saejin.png';
 import davide from '../assets/davide.png';
-import jones from '../assets/jones.png'
+import jonathan from '../assets/jones.png'
 import bren from '../assets/bren.png';
 import aaron from '../assets/aaron.png';
 import taven from '../assets/taven.png';
@@ -19,67 +19,91 @@ import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import { link } from 'fs';
 import { faTruckPickup } from '@fortawesome/free-solid-svg-icons';
 
-
-const linkedinIcon = <a href=""><FontAwesomeIcon icon={faLinkedin} className="social-icons linkedin"/></a>
-const github = <a href=""><FontAwesomeIcon icon={faGithubSquare} className="social-icons github"/></a>
-
 export default function Team() {
     const pictures = [
         { 
             src : saejin,
             alt : 'saejin',
+            linkedin : 'https://github.com/skang1004',
+            github : 'https://www.linkedin.com/in/saejinkang1004/'
         },
         {
-            src : jones,
-            alt : 'jones'
+            src : jonathan,
+            alt : 'jonathan',
+            linkedin : 'https://www.linkedin.com/in/jon-escamilla/',
+            github : 'https://github.com/jonescamilla'
         },
         {
             src : bren,
-            alt : 'bren'
+            alt : 'bren',
+            linkedin : 'https://www.linkedin.com/in/brenyamaguchi/',
+            github : 'https://github.com/brenyama'
         },
         {
             src : sean,
-            alt : 'sean'
+            alt : 'sean',
+            linkedin : 'https://www.linkedin.com/in/sean-smith17/',
+            github : 'https://github.com/SmithSean17'
         },
         {
             src : henry,
-            alt : 'henry'
+            alt : 'henry',
+            linkedin : 'https://www.linkedin.com/in/henrytaing/',
+            github : 'https://github.com/henrytaing'
         },
         {
             src : steven,
-            alt : 'steven'
+            alt : 'steven',
+            linkedin : 'https://github.com/Steven-Nguyen-T',
+            github : 'https://www.linkedin.com/in/steven-nguyen-t/'
         },
         {
             src : spenser,
-            alt : 'spenser'
+            alt : 'spenser',
+            linkedin : 'https://www.linkedin.com/in/spenser-schwartz/',
+            github : 'https://github.com/spenserschwartz'
         },
         {
             src : seungho,
-            alt : 'seungho'
+            alt : 'seungho',
+            linkedin : 'https://www.linkedin.com/in/s2unghobaek/',
+            github : 'https://github.com/hobaek'
         },
         {
             src : justin,
-            alt : 'justin'
+            alt : 'justin',
+            linkedin : 'https://www.linkedin.com/in/justinchoo93/',
+            github : 'https://github.com/justinchoo93'
         },
         {
             src : anthony,
-            alt : 'anthony'
+            alt : 'anthony',
+            linkedin : 'https://www.linkedin.com/in/anthony-lin/',
+            github : 'https://github.com/anthonylin198'
         },
         {
             src : aaron,
-            alt : 'aaron'
+            alt : 'aaron',
+            linkedin : 'https://www.linkedin.com/in/aaron-yang-393616160/',
+            github : 'https://github.com/aaronyang24'
         },
         {
             src : jesus,
-            alt : 'jesus'
+            alt : 'jesus',
+            linkedin : 'https://www.linkedin.com/in/jesus-modesto-vargas/',
+            github : 'https://github.com/jmodestov'
         },
         {
             src : taven,
-            alt : 'taven'
+            alt : 'taven',
+            linkedin : 'https://www.linkedin.com/in/taven-shumaker/',
+            github : 'https://github.com/TavenShumaker'
         },
         {
             src : davide,
-            alt : 'davide'
+            alt : 'davide',
+            linkedin : 'https://www.linkedin.com/in/davide-molino/',
+            github : 'https://github.com/davidemmolino'
         }
     ];
 
@@ -88,6 +112,9 @@ export default function Team() {
             <h2 className="section-title-right">Contributors</h2>
             <div id="contributor-pictures" className="grid-container">
                 {pictures.map((pic, i) => {
+                    const linkedinIcon = <a href={ pic.linkedin }><FontAwesomeIcon icon={faLinkedin} className="social-icons linkedin"/></a>
+                    const github = <a href={ pic.github } target="_blank" ><FontAwesomeIcon icon={faGithubSquare} className="social-icons github"/></a>
+
                     return (
                         <div>
                             <div>
