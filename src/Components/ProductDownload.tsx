a0acfab1c75c1e3c9b05d78483d6f3e6e56fa411
Jesus <jmodestov@gmail.com>
2020-10-21 22:37:00 -0700

    media query details

--- src/Components/ProductDownload.tsx
+++ src/Components/ProductDownload.tsx
@@ -4,7 +4,7 @@ export default function ProductDownload() {
   return (
     <div id='Product' className='Product light-background'>
       <div className='inner'>
-        <h2  className="section-title-center" style={{ color:  "#393b44" }}>A Chrome extension that helps debug Recoil applications by memorizing the state of components with every render.</h2 >
+        <h2 style={{ color:  "#393b44" }}>A Chrome extension that helps debug Recoil applications by memorizing the state of components with every render.</h2 >
         <div className='buttons'>
 
            <a href='#getting-started-anchor'><button className="grey-button btn-left">Get Started</button></a>
